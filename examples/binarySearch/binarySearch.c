6b6f80863fa893d67db4373137c153a3068aa341
Mahdi Fani-Disfani <mahdifani1369@gmail.com>
2016-10-31 16:38:08 +0100

    binarySearch has been annotated.

--- examples/binarySearch/binarySearch.c
+++ examples/binarySearch/binarySearch.c
@@ -1,36 +1,108 @@
 #include<stdio.h>
-int main(){
+#include <stdlib.h>
 
-    int a[10],i,n,m,c=0,l,u,mid;
 
-    printf("Enter the size of an array: ");
-    scanf("%d",&n);
+void sort(int* number, int n{
 
-    printf("Enter the elements in ascending order: ");
-    for(i=0;i<n;i++){
-         scanf("%d",&a[i]);
+ /*Sort the given array number , of length n*/
+
+int temp=0,j,i;
+    for(i=1;i<n;i++)
+    {
+        for(j=0;j<n-i;j++)
+        {
+            if(number[j] >number[j+1])
+            {
+                temp=number[j];
+                number[j]=number[j+1];
+                number[j+1]=temp;
+            }
+        }
     }
+}
 
-    printf("Enter the number to be search: ");
-    scanf("%d",&m);
+void binarySearch(int N, int *y){
 
+  /*@ begin PerfTuning (
+   def build {
+     arg build_command = 'gcc';
+     #arg libs = '-lrt';  # Only needed on linux
+   }
+   def performance_counter {
+     arg repetitions = 5;
+   }
+   def performance_params {
+     param UF[] = range(1,11);
+     param CFLAGS[] = ['-O0', '-O1', '-O2', '-O3'];
+     #constraint divisible_by_two = (UF % 2 == 0);
+   }
+   def input_params {
+     param N[] = [10000];
+   }
+   def input_vars {
+     decl dynamic int y[N] = 0;
+   }
+   def search {
+   arg algorithm = 'Exhaustive';
+ }
+) @*/
+
+    int a[100],m,l,u,mid;
+    int n=N;
+
+    register int i;
+
+    for(i=0 ; i <= (n-1) ; i=i+1)
+      a[i] = (rand() % 300);
+
+    sort(a, 100);
+    m = 222;
     l=0,u=n-1;
-    while(l<=u){
+
+/*@ begin Loop (
+
+	transform Unroll(ufactor=UF)
+  for(i=0 ; i <= (n-1) ; i=i+1)
+  {
+
+      if (l>u)
+        break;
+
+       mid=(l+u)/2;
+       if(m==a[mid])
+       {
+           y[0] = 1;
+           break;
+       }
+       else if(m<a[mid])
+       {
+           u=mid-1;
+       }
+       else
+           l=mid+1;
+  }
+) @*/
+
+    for(i=0 ; i <= (n-1) ; i=i+1)
+    {
+
+        if (l>u)
+          break;
+
          mid=(l+u)/2;
-         if(m==a[mid]){
-             c=1;
+         if(m==a[mid])
+         {
+             y[0] = 1;
              break;
          }
-         else if(m<a[mid]){
+         else if(m<a[mid])
+         {
              u=mid-1;
          }
          else
              l=mid+1;
     }
-    if(c==0)
-         printf("The number is not found.");
-    else
-         printf("The number is found.");
 
-    return 0;
+/*@ end @*/
+/*@ end @*/
 }
