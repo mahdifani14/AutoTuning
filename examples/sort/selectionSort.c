93895e1457493b599b3af7c7736475a2b8614999
Mahdi Fani-Disfani <mahdifani1369@gmail.com>
2016-12-08 17:22:04 +0100

    Sort code has been edited.

--- examples/sort/selectionSort.c
+++ examples/sort/selectionSort.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
- 
+
 int main()
 {
    int array[100], n, c, d, position, swap;
- 
+
 //   printf("Enter %d integers\n", n);
- 
+
    for ( c = 0 ; c < 100 ; c++ )
    {
       array[c] = (rand() % 300);
-//      printf("%d\t", array[c]);
+     printf("%d\t", array[c]);
    }
- 
+   printf("\n\n----------------\n");
    for ( c = 0 ; c < ( 100 - 1 ) ; c++ )
    {
       position = c;
- 
+
       for ( d = c + 1 ; d < 100 ; d++ )
       {
          if ( array[position] > array[d] )
@@ -29,12 +29,12 @@ int main()
          array[position] = swap;
       }
    }
- 
+
 //   printf("\nSorted list in ascending order:\n");
- 
+
    for ( c = 0 ; c < 100 ; c++ )
       printf("%d\t", array[c]);
 
-//   printf("\n"); 
+//   printf("\n");
    return 0;
 }
