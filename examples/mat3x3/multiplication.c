7050c7e90709b28ea4e26967ae1b9968ba2e4f18
Mahdi Fani-Disfani <mahdifani1369@gmail.com>
2016-12-08 17:41:31 +0100

    Mat3x3 code has been edited.

--- examples/mat3x3/multiplication.c
+++ examples/mat3x3/multiplication.c
@@ -1,5 +1,16 @@
 #include <stdio.h>
-#include <stdlib.h>
+
+#define SIZE 100
+
+int input[SIZE] = {283,286,177,115,293,235,286,192,249,121,62,127,290,259,
+                263,226,240,126,172,136,11,68,267,129,182,230,62,223,67,
+                235,29,102,222,258,269,267,193,256,211,142,129,273,21,119,
+                284,37,98,224,215,270,113,226,91,80,56,273,62,170,196,181,
+                105,225,284,236,46,5,129,13,257,24,195,282,45,14,267,134,
+                164,243,50,187,108,276,278,188,84,3,251,254,199,132,56,176,
+                168,139,112,226,186,294,239,27};
+
+int y[SIZE];
 
 void mat3x3(int N, int *y){
 
@@ -29,17 +40,17 @@ void mat3x3(int N, int *y){
 
   int d, k, sum = 0;
   int first[10][10], second[10][10], multiply[10][10];
-  int n=N;
+  int n=N/10;
 
   register int i;
 
   for (i = 0; i <= (n-1); i=i+1)
     for (d = 0; d <= (n-1); d=d+1)
-      first[i][d] = (rand() % 100);
+      first[i][d] = input[i];
 
     for (i = 0; i <= (n-1); i=i+1)
       for (d = 0; d <= (n-1); d=d+1)
-      second[i][d] = (rand() % 102);
+      second[i][d] = input[i];
 
 // int i;
 /*@ begin Loop (
@@ -76,4 +87,16 @@ void mat3x3(int N, int *y){
 
 /*@ end @*/
 /*@ end @*/
+}
+
+
+int main(void) {
+
+  mat3x3(SIZE,y);
+
+  register int i;
+  for ( i = 0 ; i < SIZE ; i=i+1 )
+     printf("%d\t", y[i]);
+
+return 0;
 }
